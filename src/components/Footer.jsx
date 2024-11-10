6f8db7eb772075c9ccc10543b764d6b6b1635704
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-11-10 10:35:00 +0530

    shadow added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <p className="text-purple-700 text-center my-4">Happy cooking ❤</p>;
+  return <p className="text-purple-700 text-center my-4 drop-shadow-md">Happy cooking ❤</p>;
 };
 
 export default Footer;
