ae22d476a8a606cabf47e3face73125c3e4a037e
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-11-10 14:31:32 +0530

    Footer UI fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <p className="text-purple-700 text-center my-4 drop-shadow-md">Happy cooking ❤</p>;
+  return (
+    <p className="text-purple-700 text-center mt-4 drop-shadow-md w-full py-2 bg-purple-100 shadow-md">
+      Happy cooking ❤
+    </p>
+  );
 };
 
 export default Footer;
